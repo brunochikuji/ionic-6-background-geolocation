a3a1ae08059994a0537a04139ef4a8b02bb6859d
Bruno Chikuji <bruno.chikuji@totvs.com.br>
2022-07-28 15:57:59 -0300

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { BackgroundGeolocationLocationProvider } from '@awesome-cordova-plugins/background-geolocation/ngx';
+import {
+  BackgroundGeolocation,
+  BackgroundGeolocationConfig,
+  BackgroundGeolocationEvents,
+  BackgroundGeolocationResponse,
+} from '@awesome-cordova-plugins/background-geolocation/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(
+    private backgroundGeolocation: BackgroundGeolocation,
+    private httpClient: HttpClient,
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+  configurar() {
+    const config: BackgroundGeolocationConfig = {
+      locationProvider: BackgroundGeolocationLocationProvider.RAW_PROVIDER,
+      desiredAccuracy: 10,
+      stationaryRadius: 1,
+      distanceFilter: 1,
+      debug: true, //  enable this hear sounds for background-geolocation life-cycle.
+      stopOnTerminate: true, // enable this to clear background location settings when the app terminates,
+      startForeground: true,
+      interval: 5000,
+      fastestInterval: 5000,
+      activitiesInterval: 5000,
+      notificationTitle: 'atualizado',
+    };
+    this.backgroundGeolocation.configure(config).then(() => {
+      this.backgroundGeolocation.on(BackgroundGeolocationEvents.location).subscribe((location: BackgroundGeolocationResponse) => {
+        console.log('Veio, aqui você pode fazer a chamada http ou chamar outros metodos!', location);
+        console.log('Importante, só vai ter uma chamada nesse evento se a localização do usuário mudar!');
+      });
+    });
+  }
+
+  stop() {
+    this.backgroundGeolocation.stop();
+  }
+
+
+  start() {
+    this.backgroundGeolocation.start();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BackgroundGeolocation } from '@awesome-cordova-plugins/background-geolocation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, BackgroundGeolocation],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
